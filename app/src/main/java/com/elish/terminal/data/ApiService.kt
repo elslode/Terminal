d595917f02b5eedc6ba0847753eaf38858238b7d
Elisey Vavulin <elisey.vavulin@lenta.com>
2023-11-26 15:01:49 +0100

    TER_3: Добавил зум и ограничил показ свечей на графике при максимальном и минимальном зуме

--- app/src/main/java/com/elish/terminal/data/ApiService.kt
+++ app/src/main/java/com/elish/terminal/data/ApiService.kt
@@ -4,6 +4,6 @@ import retrofit2.http.GET
 
 interface ApiService {
 
-    @GET("aggs/ticker/AAPL/range/1/hour/2022-01-09/2023-01-09?adjusted=true&sort=asc&limit=50000&apiKey=vv25ipCpwwaVewSXVKJ9cTC8mZkOoT5M")
+    @GET("aggs/ticker/AAPL/range/1/hour/2022-01-09/2023-01-09?adjusted=true&sort=desc&limit=50000&apiKey=vv25ipCpwwaVewSXVKJ9cTC8mZkOoT5M")
     suspend fun loadBars(): Result
 }
